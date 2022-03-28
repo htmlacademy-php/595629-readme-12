6c4caf821ddd6f8e0200ab64d8a205be0120d005
lemon <s.plyakin@yandex.ru>
2022-03-28 09:45:50 +0200

    2.5. добавили href="http://<?= $post['content'] ?>"

--- index.php
+++ index.php
@@ -344,7 +344,7 @@ $user_name = 'Сергей'; // укажите здесь ваше имя
                         <?php elseif ($post['type'] == 'post-link'): ?>
                             <!--содержимое для поста-ссылки-->
                             <div class="post-link__wrapper">
-                                <a class="post-link__external" href="http://" title="Перейти по ссылке">
+                                <a class="post-link__external" href="http://<?= $post['content'] ?>" title="Перейти по ссылке">
                                     <div class="post-link__info-wrapper">
                                         <div class="post-link__icon-wrapper">
                                             <img src="https://www.google.com/s2/favicons?domain=vitadental.ru"
